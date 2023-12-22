20c94f77a7d5a1cae7e87387c9f514a0f614fdaf
= <ramirezpineda.rickyroy@gmail.com>
2023-12-22 10:49:09 -0400

    changes

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ from typing import Union
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
+import smtplib
+from email.mime.text import MIMEText
+
 from pydantic import BaseModel
 
 from chat import chat
@@ -14,6 +17,9 @@ origins = [
     "http://localhost:8080",
     "http://localhost:8000",
     "http://localhost:5173",
+    "http://localhost:5500",
+    "http://127.0.0.1:5500",
+    "http://localhost:5500/index.html",
     "https://cdam-web-site.onrender.com",
 ]
 
@@ -41,3 +47,27 @@ def read_item(item_id: int, q: Union[str, None] = None):
 def chatbot(message: Message):
     response = chat(message.message)
     return {"response": response}
+
+
+class EmailData(BaseModel):
+    name: str
+    email: str
+    subject: str
+    message: str
+
+@app.post("/send-email")
+async def send_email(email_data: EmailData):
+
+    sender = "Private Person <" + email_data.email + ">"
+    receiver = "User <ramirez.ricky2021@example.com>"
+
+    message = MIMEText(email_data.message)
+    message["Subject"] = email_data.subject
+    message["From"] = sender
+    message["To"] = receiver
+
+    with smtplib.SMTP("sandbox.smtp.mailtrap.io", 2525) as server:
+        server.login("d95dba990663a3", "bf8de2d3da745a")
+        server.sendmail(sender, receiver, message.as_string())
+
+    return {"message": "Email sent successfully"}
