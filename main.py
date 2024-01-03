acfff86f53b81620ca4904e469a75c9a39423bce
Ramirez Pineda Ricky Roy <86938625+RamirezPineda@users.noreply.github.com>
2024-01-03 15:10:52 -0400

    Update main.py

--- main.py
+++ main.py
@@ -12,21 +12,10 @@ from chat import chat
 
 app = FastAPI()
 
-origins = [
-    "http://localhost",
-    "http://localhost:8080",
-    "http://localhost:8000",
-    "http://localhost:5173",
-    "http://localhost:5500",
-    "http://127.0.0.1:5500",
-    "http://localhost:5500/index.html",
-    "https://cdam-web-site.onrender.com",
-]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
+    allow_origins=["*"],
     allow_methods=["*"],
     allow_headers=["*"],
 )
